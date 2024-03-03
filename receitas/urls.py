15c6daa859e9d70b21f4d07defc00c662db2e9dc
Wellington Souza <111922665+BGWellSS@users.noreply.github.com>
2024-03-03 00:32:52 -0300

    Pagina de detalhes da receita

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from receitas.views import index
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    # https://docs.djangoproject.com/pt-br/3.2/topics/http/urls/
+    path('receita/<int:id>/', views.receita, name='receita'),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -6,3 +6,9 @@ def index(request):
     return render(request, 'receitas/index.html', context={
         'nome_receita': 'Lasanha',
     }, status=200)
+
+
+def receita(request, id):
+    return render(request, 'receitas/pages/receita-view.html', context={
+        'nome_receita': 'pizza',
+    }, status=200)
