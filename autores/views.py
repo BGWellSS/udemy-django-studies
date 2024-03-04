b17148d4f6b2ee9aa7963b86eece31a95327db35
Wellington Souza <111922665+BGWellSS@users.noreply.github.com>
2024-03-04 10:52:34 -0300

    Templates de 'autores' e formulario de cadastro inicial

--- autores/views.py
+++ autores/views.py
@@ -5,12 +5,19 @@ from .forms import CadastroForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'autores/pages/index.html')
+    return render(request, 'autores/pages/index.html', context={
+        'titulo': 'Autores - Home',
+    }, status=200)
 
 
 def cadastro(request):
-    formulario = CadastroForm()
+
+    if request.POST:
+        formulario = CadastroForm(request.POST)
+    else:
+        formulario = CadastroForm()
 
     return render(request, 'autores/pages/cadastro.html', context={
+        'titulo': 'Autores - Cadastro',
         'formulario': formulario
     }, status=200)
